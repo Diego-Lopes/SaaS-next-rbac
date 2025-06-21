651629c35d650d8e0102437b07b15b18413c41fd
Diego Lopes <diegolopes2009@hotmail.com>
2025-06-21 12:08:04 -0400

    ✨feat: created backend

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { ability } from '@saas/auth'
-
-const useCanInviteSomeoneElse = ability.can('invite', 'all')
-
-console.log(`Can invite someone else: ${useCanInviteSomeoneElse}`)
-
-const useCannotDeleteOtherUsers = ability.cannot('delete', 'User')
-
-console.log(`Cannot delete other users: ${useCannotDeleteOtherUsers}`)
