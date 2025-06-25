9eed434dfb3ad6e1e4a3dff2967156a999f7dfb0
Diego-Lopes <diegolopes2009@hotmail.com>
2025-06-25 01:23:04 -0400

    ✨feat: env package created

--- apps/api/src/http/routes/auth/authenticate-with-github.ts
+++ apps/api/src/http/routes/auth/authenticate-with-github.ts
@@ -1,3 +1,4 @@
+import { env } from '@saas/env'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
@@ -32,14 +33,14 @@ export async function authenticateWithGithub(app: FastifyInstance) {
         'https://github.com/login/oauth/access_token',
       )
 
-      githubOAuthUrl.searchParams.set('client_id', 'Ov23liZZ7gM4IAZY27br')
+      githubOAuthUrl.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
       githubOAuthUrl.searchParams.set(
         'client_secret',
-        'babee2959d70c4d50b4a50eaf2040d76ed3f4c63',
+        env.GITHUB_OAUTH_CLIENT_SECRET,
       )
       githubOAuthUrl.searchParams.set(
         'redirect_uri',
-        'http://localhost:3000/api/auth/callback',
+        env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
       )
       githubOAuthUrl.searchParams.set('code', code)
       //  'https://github.com/login/oauth/authorize?client_id=Ov23liZZ7gM4IAZY27br&redirect_uri=http://localhost:3000/api/auth/callback&scope=user:email'
