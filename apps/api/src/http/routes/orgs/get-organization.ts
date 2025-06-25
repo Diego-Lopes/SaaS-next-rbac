2168138a6981476520d83d537a0746bcb0d2aa54
Diego Lopes <diegolopes2009@hotmail.com>
2025-06-25 18:03:16 -0400

    ✨feat: create routes organization, organizations

---
+++ apps/api/src/http/routes/orgs/get-organization.ts
@@ -0,0 +1,49 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+
+const getOrganizationsSchema = z.object({
+  slug: z.string(),
+})
+
+export async function getOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organization/:slug',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get details from organization',
+          security: [{ bearerAuth: [] }],
+          params: getOrganizationsSchema,
+          response: {
+            200: z.object({
+              id: z.string().uuid(),
+              name: z.string(),
+              slug: z.string(),
+              domain: z.string().nullable(),
+              shouldAttachUsersByDomain: z.boolean(),
+              avatarUrl: z.string().url().nullable(),
+              createdAt: z.date(),
+              updatedAt: z.date(),
+              ownerId: z.string().uuid(),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params as z.infer<
+          typeof getOrganizationsSchema
+        >
+        const { organization } = await request.getUserMembership(slug)
+
+        return {
+          organization,
+        }
+      },
+    )
+}
---
+++ apps/api/src/http/routes/orgs/get-organizations.ts
@@ -0,0 +1,79 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+const getOrganizationsSchema = z.object({
+  slug: z.string(),
+})
+
+export async function getOrganizations(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get details where user is member',
+          security: [{ bearerAuth: [] }],
+          response: {
+            200: z.object({
+              id: z.string().uuid(),
+              name: z.string(),
+              slug: z.string(),
+              domain: z.string().nullable(),
+              shouldAttachUsersByDomain: z.boolean(),
+              avatarUrl: z.string().url().nullable(),
+              createdAt: z.date(),
+              updatedAt: z.date(),
+              ownerId: z.string().uuid(),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const userId = await request.getCurrentUserId()
+        const organizations = await prisma.organization.findMany({
+          select: {
+            id: true,
+            name: true,
+            slug: true,
+            avatarUrl: true,
+            members: {
+              // buscar organizações onde o usuário é membro e o tipo de membro
+              select: {
+                role: true,
+              },
+              where: {
+                userId,
+              },
+            },
+          },
+          where: {
+            members: {
+              some: {
+                userId,
+              },
+            },
+          },
+        })
+
+        const organizationsWithUserRole = organizations.map(
+          ({ members, ...org }) => {
+            return {
+              ...org,
+              role: members[0].role, // assumindo que o usuário só tem um papel por organização
+            }
+          },
+        )
+
+        return {
+          organizations,
+        }
+      },
+    )
+}
