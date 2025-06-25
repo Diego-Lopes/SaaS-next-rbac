bb84d65a9ec8d32111ecdbe40e79460d3bbcf739
Diego-Lopes <diegolopes2009@hotmail.com>
2025-06-25 01:58:03 -0400

    ✨feat: create route organization

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -21,6 +21,7 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecorver } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { createOrganization } from './routes/orgs/create-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -72,12 +73,16 @@ app.register(fastifyjwt, {
 })
 
 // registrando as rotas
+// routes Auth
 app.register(createAccount)
 app.register(authenticateWithPassword)
 app.register(getProfile)
 app.register(requestPasswordRecorver)
 app.register(resetPassword)
 app.register(authenticateWithGithub)
+
+// routes Org
+app.register(createOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(
