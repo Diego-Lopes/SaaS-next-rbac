b5443766064abfbeab776d96632cf4115128037e
Diego Lopes <diegolopes2009@hotmail.com>
2025-06-26 17:53:14 -0400

    ♻️refactore: created route shutdown organization and fix this in package auth

--- apps/api/src/http/routes/orgs/shutdown-organization.ts
+++ apps/api/src/http/routes/orgs/shutdown-organization.ts
@@ -7,36 +7,27 @@ import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
 import { getUserPermissions } from '@/utils/get-user-permissions'
 
-import { BadRequestError } from '../_erros/bad-request-error'
 import { UnauthorizedError } from '../_erros/unauthorized-error'
-
-const updateOrganizatioSchema = z.object({
-  name: z.string(),
-  domain: z.string().nullish(),
-  shouldAttachUsersByDomain: z.boolean().optional(),
-})
 
 const paramsOrganizationSchema = z.object({
   slug: z.string(),
 })
 
-export async function updateOrganization(app: FastifyInstance) {
+export async function shutdownOrganization(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
-    .put(
+    .delete(
       '/organization/:slug',
       {
         schema: {
           tags: ['organizations'],
-          summary: 'Update organizaton details',
+          summary: 'Shutdown organization',
           security: [{ bearerAuth: [] }],
-          body: updateOrganizatioSchema,
+          // body: updateOrganizatioSchema,
           params: paramsOrganizationSchema,
           response: {
-            204: z.object({
-              organizationId: z.string().uuid(),
-            }),
+            204: z.null(),
           },
         },
       },
@@ -49,11 +40,6 @@ export async function updateOrganization(app: FastifyInstance) {
         const { membership, organization } =
           await request.getUserMembership(slug)
 
-        const { name, domain, shouldAttachUsersByDomain } =
-          request.body as z.infer<typeof updateOrganizatioSchema>
-
-        // verifica se o usuário tem permissão de atualizar a organização
-
         // Obtendo a organização baseado no organizationSchema
         // como o objeto organization já é do tipo Organization iguais então passo o objeto e o zod utiliza o que precisa
         const authOrganization = organizationSchema.parse(organization)
@@ -61,37 +47,15 @@ export async function updateOrganization(app: FastifyInstance) {
         // validando permissões com casl
         const { cannot } = getUserPermissions(userId, membership.role)
 
-        if (cannot('update', authOrganization)) {
+        if (cannot('delete', authOrganization)) {
           throw new UnauthorizedError(
-            "You're not allowed to update this organization.",
+            "You're not allowed to shutdown this organization.",
           )
         }
 
-        if (domain) {
-          const organizationByDomain = await prisma.organization.findFirst({
-            where: {
-              domain,
-              id: {
-                not: organization.id, // não pode ser a própria organização
-              },
-            },
-          })
-
-          if (organizationByDomain) {
-            throw new BadRequestError(
-              'Another organization with same domanin already exists.',
-            )
-          }
-        }
-
-        await prisma.organization.update({
+        await prisma.organization.delete({
           where: {
             id: organization.id,
-          },
-          data: {
-            name,
-            domain,
-            shouldAttachUsersByDomain,
           },
         })
 
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -47,11 +47,16 @@ export function defineAbilityFor(user: User) {
   // caso contrario passamos o role para a funcao de permissão
   permission[user.role](user, builder)
 
+  // estamos usando esse objeto para definir as regras de permissão do usuário e usamos no .bind() logo abaixo.
   const ability = builder.build({
     detectSubjectType(subject) {
       return subject.__typename // estamos dizendo ao casl qual é a subject do projeto da models project.ts
     },
   })
+
+  // quando desestruturamos o ability, ele perde a referência do this para corrigir usamos .bind() que significa setar para sempre algo.
+  ability.can = ability.can.bind(ability)
+  ability.cannot = ability.cannot.bind(ability)
 
   return ability
 }
