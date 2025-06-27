2e94cc6f86635ee65cae38a91d635cf4cff9ae87
Diego Lopes <diegolopes2009@hotmail.com>
2025-06-27 15:49:53 -0400

    ✨feat: create route accept invite

---
+++ apps/api/src/http/routes/invites/accept-invite.ts
@@ -0,0 +1,78 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_erros/bad-request-error'
+
+const paramsSchema = z.object({
+  inviteId: z.string().uuid(),
+})
+
+export async function acceptInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/invites/:inviteId/accept',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Accept an invite',
+          params: paramsSchema,
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, replay) => {
+        const userId = await request.getCurrentUserId()
+        const { inviteId } = request.params as z.infer<typeof paramsSchema>
+
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+          },
+        })
+
+        if (!invite) {
+          throw new BadRequestError('Invite not found or expired.')
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User not found.')
+        }
+
+        // se o email do convite for diferente do email do usuário, não pode aceitar o convite
+        if (invite.email !== user.email) {
+          throw new BadRequestError('This invite belongs to another user.')
+        }
+
+        await prisma.$transaction([
+          prisma.member.create({
+            data: {
+              userId,
+              organizationId: invite.organizationId,
+              role: invite.role,
+            },
+          }),
+
+          prisma.invite.delete({
+            where: {
+              id: inviteId,
+            },
+          }),
+        ])
+
+        return replay.status(204).send()
+      },
+    )
+}
