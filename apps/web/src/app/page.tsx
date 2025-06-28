b286c1b66bdb6896c601bec6d4da9f9a85c7a11f
Diego Lopes <diegolopes2009@hotmail.com>
2025-06-28 12:02:26 -0400

    ✨feat: create page sign up

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,18 @@
+import { Alert } from '@/components/ui/alert'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Separator />
+      <Button>Clique aqui</Button>
+      <Input />
+      <Label>asldkfjlasjdlf</Label>
+      <Alert>asdfasdf</Alert>
+    </>
+  )
 }
