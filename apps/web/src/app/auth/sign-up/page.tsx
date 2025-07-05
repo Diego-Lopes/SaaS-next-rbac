8e21ff370b348e8083065ab5d1dc4de3ab1d43f3
Diego-Lopes <diegolopes2009@hotmail.com>
2025-07-04 22:09:41 -0400

    ✨feat: created page sign up

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -1,54 +1,5 @@
-import Link from 'next/link'
-
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Separator } from '@/components/ui/separator'
+import { SignUpForm } from './sign-up-form'
 
 export default function SignUpPage() {
-  return (
-    <form action="" className="space-y-4">
-      <div className="space-y-1">
-        <Label htmlFor="name">Name</Label>
-        <Input name="name" id="name" />
-      </div>
-      <div className="space-y-1">
-        <Label htmlFor="email">E-mail</Label>
-        <Input name="email" type="email" id="email" />
-      </div>
-      <div className="space-y-1">
-        <Label htmlFor="password">Password</Label>
-        <Input name="password" type="password" id="password" />
-      </div>
-      <div className="space-y-1">
-        <Label htmlFor="password_confirmation">Confirm your password</Label>
-        <Input
-          name="password_confirmation"
-          type="password"
-          id="password_confirmation"
-        />
-      </div>
-      <Button type="submit" className="w-full">
-        Create account
-      </Button>
-      <Button variant={'link'} className="w-full" asChild size={'sm'}>
-        <Link href={'/auth/sign-in'}>Already registered? Sign in</Link>
-      </Button>
-      <Separator />
-      <Button variant={'outline'} type="submit" className="w-full">
-        <svg
-          aria-hidden="true"
-          height="24"
-          viewBox="0 0 24 24"
-          version="1.1"
-          width="24"
-          data-view-component="true"
-          className="mr-2 size-4 dark:invert"
-        >
-          <path d="M12 1C5.923 1 1 5.923 1 12c0 4.867 3.149 8.979 7.521 10.436.55.096.756-.233.756-.522 0-.262-.013-1.128-.013-2.049-2.764.509-3.479-.674-3.699-1.292-.124-.317-.66-1.293-1.127-1.554-.385-.207-.936-.715-.014-.729.866-.014 1.485.797 1.691 1.128.99 1.663 2.571 1.196 3.204.907.096-.715.385-1.196.701-1.471-2.448-.275-5.005-1.224-5.005-5.432 0-1.196.426-2.186 1.128-2.956-.111-.275-.496-1.402.11-2.915 0 0 .921-.288 3.024 1.128a10.193 10.193 0 0 1 2.75-.371c.936 0 1.871.123 2.75.371 2.104-1.43 3.025-1.128 3.025-1.128.605 1.513.221 2.64.111 2.915.701.77 1.127 1.747 1.127 2.956 0 4.222-2.571 5.157-5.019 5.432.399.344.743 1.004.743 2.035 0 1.471-.014 2.654-.014 3.025 0 .289.206.632.756.522C19.851 20.979 23 16.854 23 12c0-6.077-4.922-11-11-11Z"></path>
-        </svg>
-        Sign up with GitHub
-      </Button>
-    </form>
-  )
+  return <SignUpForm />
 }
--- apps/web/src/app/auth/sign-up/sign-up-form.tsx
+++ apps/web/src/app/auth/sign-up/sign-up-form.tsx
@@ -12,28 +12,17 @@ import { Separator } from '@/components/ui/separator'
 import { useFormState } from '@/hooks/use-form-state'
 
 import { signInWithGithub } from '../actions'
-import { signInWithEmailAndPassword } from './actions'
+import { signUpAction } from './actions'
 
-export function SignInForm() {
-  /**
-   * state ele tem a função salvar o estado passado
-   * nosso modelo abaixo, executamos primeira vez, e vai receber null como passamos no stado inicial de use Action state
-   * depois se não demor um reload na página temos o estado anterior, que é um secesso! que é retornado do
-   * sign in with e-mail e password
-   */
-  // const [state, formAction, isPending] = useActionState(
-  //   signInWithEmailAndPassword,
-  //   { success: false, message: null, errors: null },
-  // )
+export function SignUpForm() {
+  const route = useRouter()
 
-  const router = useRouter() // useRouter usa-se no client
   const [{ success, message, errors }, handleSubmit, isPending] = useFormState(
-    signInWithEmailAndPassword,
+    signUpAction,
     () => {
-      router.push('/')
+      route.push('/auth/sign-in')
     },
   )
-
   return (
     <div className="space-y-4">
       <form onSubmit={handleSubmit} className="space-y-4">
@@ -46,6 +35,15 @@ export function SignInForm() {
             </AlertDescription>
           </Alert>
         )}
+        <div className="space-y-1">
+          <Label htmlFor="name">Name</Label>
+          <Input name="name" id="name" />
+          {errors?.name && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.name[0]}
+            </p>
+          )}
+        </div>
         <div className="space-y-1">
           <Label htmlFor="email">E-mail</Label>
           <Input name="email" type="email" id="email" />
@@ -60,36 +58,37 @@ export function SignInForm() {
           <Input name="password" type="password" id="password" />
           {errors?.password && (
             <p className="text-xs font-medium text-red-500 dark:text-red-400">
-              {errors?.password[0]}
+              {errors.password[0]}
             </p>
           )}
-          <Link
-            href={'/auth/forgot-password'}
-            className="text-foreground text-xs font-medium hover:underline"
-          >
-            Forgot your password?
-          </Link>
+        </div>
+        <div className="space-y-1">
+          <Label htmlFor="password_confirmation">Confirm your password</Label>
+          <Input
+            name="password_confirmation"
+            type="password"
+            id="password_confirmation"
+          />
+          {errors?.password_confirmation && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.password_confirmation[0]}
+            </p>
+          )}
         </div>
         <Button type="submit" className="w-full" disabled={isPending}>
           {isPending ? (
             <Loader2 className="size-4 animate-spin" />
           ) : (
-            'Sign in with e-mail'
+            'Create account'
           )}
         </Button>
-
         <Button variant={'link'} className="w-full" asChild size={'sm'}>
-          <Link href={'/auth/sign-up'}>Create new account</Link>
+          <Link href={'/auth/sign-in'}>Already registered? Sign in</Link>
         </Button>
       </form>
       <Separator />
       <form action={signInWithGithub}>
-        <Button
-          variant={'outline'}
-          type="submit"
-          className="w-full"
-          disabled={isPending}
-        >
+        <Button variant={'outline'} type="submit" className="w-full">
           <svg
             aria-hidden="true"
             height="24"
@@ -97,11 +96,11 @@ export function SignInForm() {
             version="1.1"
             width="24"
             data-view-component="true"
-            className="dark:invert"
+            className="mr-2 size-4 dark:invert"
           >
             <path d="M12 1C5.923 1 1 5.923 1 12c0 4.867 3.149 8.979 7.521 10.436.55.096.756-.233.756-.522 0-.262-.013-1.128-.013-2.049-2.764.509-3.479-.674-3.699-1.292-.124-.317-.66-1.293-1.127-1.554-.385-.207-.936-.715-.014-.729.866-.014 1.485.797 1.691 1.128.99 1.663 2.571 1.196 3.204.907.096-.715.385-1.196.701-1.471-2.448-.275-5.005-1.224-5.005-5.432 0-1.196.426-2.186 1.128-2.956-.111-.275-.496-1.402.11-2.915 0 0 .921-.288 3.024 1.128a10.193 10.193 0 0 1 2.75-.371c.936 0 1.871.123 2.75.371 2.104-1.43 3.025-1.128 3.025-1.128.605 1.513.221 2.64.111 2.915.701.77 1.127 1.747 1.127 2.956 0 4.222-2.571 5.157-5.019 5.432.399.344.743 1.004.743 2.035 0 1.471-.014 2.654-.014 3.025 0 .289.206.632.756.522C19.851 20.979 23 16.854 23 12c0-6.077-4.922-11-11-11Z"></path>
           </svg>
-          Sign in with GitHub
+          Sign up with GitHub
         </Button>
       </form>
     </div>
