78ce87947c9c0b1e5e3fbfed867d13f73568311a
Diego-Lopes <diegolopes2009@hotmail.com>
2025-07-06 15:45:24 -0400

    ♻️refactor: added user id in get membership backend

--- apps/api/src/http/routes/orgs/get-membership.ts
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -26,6 +26,7 @@ export async function getMembership(app: FastifyInstance) {
               membership: z.object({
                 id: z.string().uuid(),
                 role: roleSchema,
+                userId: z.string().uuid(),
                 organizationId: z.string().uuid(),
               }),
             }),
@@ -40,6 +41,7 @@ export async function getMembership(app: FastifyInstance) {
           membership: {
             id: membership.id,
             role: membership.role,
+            userId: membership.userId,
             organizationId: membership.organizationId,
           },
         }
