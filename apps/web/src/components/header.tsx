9e1e3858d98249b649613f7ebf773b73c5f9edbb
Diego-Lopes <diegolopes2009@hotmail.com>
2025-07-06 09:52:04 -0400

    ✨feat: created organization switch

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,9 +1,18 @@
+import { Slash } from 'lucide-react'
+
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export function Header() {
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
-      <div className="flex items-center gap-3"></div>
+      <div className="flex items-center gap-3">
+        <div>componente de imagem</div>
+
+        <Slash className="text-border size-3 -rotate-[24deg]" />
+
+        <OrganizationSwitcher />
+      </div>
 
       <div className="flex items-center gap-4">
         <ProfileButton />
