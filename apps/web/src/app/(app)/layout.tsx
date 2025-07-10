8316a92dd339ec235756537bfef78c1d9aacd6b3
Diego-Lopes <diegolopes2009@hotmail.com>
2025-07-10 00:03:03 -0400

    🚚chore: moved header by page for layout

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation'
 
 import { isAuthenticated } from '@/auth/auth'
+import { Header } from '@/components/header'
 
 export default async function AppLayout({
   children,
@@ -11,5 +12,11 @@ export default async function AppLayout({
     // redirecionando caso tiver token nos cookies!!!
     redirect('/auth/sign-in') // redirect usa-se em client server
   }
-  return <>{children}</>
+
+  return (
+    <div className="space-y-4 py-4">
+      <Header />
+      <main className="mx-auto w-full max-w-[1200px]">{children}</main>
+    </div>
+  )
 }
