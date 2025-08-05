6a76bdfb2f23005d37313a6ad9196989bdf2da21
Diego Lopes <diegolopes2009@hotmail.com>
2025-08-05 10:29:14 -0400

    ♻️refactor: added logic get cookie invite id

--- apps/web/src/app/api/auth/callback/route.ts
+++ apps/web/src/app/api/auth/callback/route.ts
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers'
 import { type NextRequest, NextResponse } from 'next/server'
 
+import { acceptInvite } from '@/http/accept-invite'
 import { signInWithGithub } from '@/http/sign-in-with-github'
 
 export async function GET(request: NextRequest) {
@@ -29,6 +30,15 @@ export async function GET(request: NextRequest) {
     path: '/',
     maxAge: 60 * 60 * 24 * 7, // 7 dias
   })
+
+  const inviteId = cookie.get('inviteId')?.value
+
+  if (inviteId) {
+    try {
+      await acceptInvite(inviteId)
+      cookie.delete('inviteId')
+    } catch (error) { }
+  }
 
   const redirectURL = request.nextUrl.clone()
   redirectURL.pathname = '/'
